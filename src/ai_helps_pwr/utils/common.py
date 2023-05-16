078156def56a892b2e81d2ac3fa75bc86a3639ab
Joanna Waczyńska <joanna.waczynska@orange.com>
2023-05-16 08:34:49 +0200

    change cls to class_object

--- src/ai_helps_pwr/utils/common.py
+++ src/ai_helps_pwr/utils/common.py
@@ -8,12 +8,12 @@ def load_model_from_config(cfg: dict):
     """Create an object based on the specified module path and kwargs."""
     module_name, class_name = cfg["module"].rsplit(".", maxsplit=1)
 
-    cls = getattr(
+    class_object = getattr(
         importlib.import_module(module_name),
         class_name,
     )
 
-    return cls(**cfg["kwargs"])
+    return class_object(**cfg["kwargs"])
 
 
 def load_prompt(path_to_json: Path):
